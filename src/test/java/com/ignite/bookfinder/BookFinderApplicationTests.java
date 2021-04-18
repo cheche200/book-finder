d71c7f89524556775d73ed439706bd229d57b7c2
Jose <joseenrique86@gmail.com>
2021-04-18 10:29:43 +0100

    Added test to parse Json payload from oreilly

--- src/test/java/com/ignite/bookfinder/BookFinderApplicationTests.java
+++ src/test/java/com/ignite/bookfinder/BookFinderApplicationTests.java
@@ -1,13 +1,39 @@
 package com.ignite.bookfinder;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ignite.bookfinder.domain.oreilly.ResultWrapper;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class BookFinderApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void parsingJsonTest() {
+        List<ResultWrapper> resultWrappers = new ArrayList<>();
+        ObjectMapper mapper = new ObjectMapper();
+        TypeReference<List<ResultWrapper>> typeReference = new TypeReference<List<ResultWrapper>>() {
+        };
+        InputStream inputStream = TypeReference.class.getResourceAsStream("/json/book.json");
+
+        try {
+            resultWrappers = mapper.readValue(inputStream, typeReference);
+            System.out.println(resultWrappers.get(0));
+        } catch (IOException e) {
+            System.out.println("Error" + e.getMessage());
+        }
+
+        assertFalse(resultWrappers.isEmpty());
+        assertTrue(resultWrappers.size() == 1);
+        assertEquals("https://www.safaribooksonline.com/api/v1/book/9781492071266/",
+                resultWrappers.get(0).getResults().get(0).getId());
+
+    }
 
 }
